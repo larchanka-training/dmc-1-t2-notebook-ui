60bcd49c25f85ae0113c174fc51439502fa7fa11
Larisa Morozhnikova <larisa.moroz@gmail.com>
2026-05-26 23:32:22 +0800

    TARDIS-70: Remove _spike debug route and worker from production bundle

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,6 @@ import '@/pages/login'
 import '@/pages/about'
 import '@/pages/shadcn-components'
 import '@/pages/custom-components'
-import '@/features/notebook/_spike/spike-route'
 
 const App = reatomComponent(() => rootRoute.render(), 'App')
 
